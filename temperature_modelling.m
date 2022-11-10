58287c317ac48144c4c75afdaa878c12f92b2916
Luca Ghislotti <ghislottiluca@gmail.com>
2022-11-10 10:14:43 +0100

    Added all computed data for all pts, temperatures and channels

--- temperature_modelling.m
+++ temperature_modelling.m
@@ -9,7 +9,7 @@ end
 
 %% Acquire FDT data at different temperatures
 
-data_sample = readtable("C:\Users\ghisl\Documents\GitHub\GAPS-FDT_temperature_model\fdt_data_temp\TransferFunction0C.dat");
+data_sample = readtable("fdt_data_raw\TransferFunction0C.dat");
 dac_inj_values = unique(data_sample.CAL_V);
 channels = [0:31]; % Chosen by user
 temperatures = [[-40:2:-30] [-20:10:30]];
@@ -18,10 +18,20 @@ peaking_times = [0:7];
 % Acquire FDT data for all temperatures at every peaking time for every
 % channel
 for temp = temperatures
-    data_raw_table = readtable("C:\Users\ghisl\Documents\GitHub\GAPS-FDT_temperature_model\fdt_data_temp\TransferFunction" + string(temp) + "C.dat");
+    data_raw_table = readtable("fdt_data_raw\TransferFunction" + string(temp) + "C.dat");
+    allch_pt_fdt_means = nan(length(dac_inj_values), length(channels));
+    allch_pt_fdt_stds = nan(length(dac_inj_values), length(channels));
     for pt = peaking_times
-        
+        for ch = channels
+            data_raw_table_pt_ch_mean = data_raw_table.mean(data_raw_table.pt == pt & data_raw_table.ch == ch);
+            data_raw_table_pt_ch_std = data_raw_table.std(data_raw_table.pt == pt & data_raw_table.ch == ch);
+            allch_pt_fdt_means(:, ch+1) = data_raw_table_pt_ch_mean;
+            allch_pt_fdt_stds(:, ch+1) = data_raw_table_pt_ch_std;
+        end 
+        % One file for every combination of peaking time and temperature
+        % DAC_inj on rows, channel on columns
+        writetable(array2table(allch_pt_fdt_means), "fdt_data_computed\means\fdt_allch_means_pt" + string(pt) + "_" + string(temp) + "C.dat", "Delimiter", "\t");
+        writetable(array2table(allch_pt_fdt_stds), "fdt_data_computed\stds\fdt_allch_stds_pt" + string(pt) + "_" + string(temp) + "C.dat", "Delimiter", "\t");
     end
 end
-
 
